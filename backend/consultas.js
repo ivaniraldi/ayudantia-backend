c51da52fb6e443bd171da96181a49de2862ce077
ivaniraldi <iraldiban@gmail.com>
2024-12-07 19:31:47 -0300

    Backend terminado

--- backend/consultas.js
+++ backend/consultas.js
@@ -8,4 +8,91 @@ const getDate = async () => {
     return result.rows[0].now
 }
 
-module.exports = { getDate }
+//Consultas de los viajes
+
+const getViajes = async () => {
+    const consulta = 'SELECT * FROM viajes';
+    const result = await pool.query(consulta);
+    return result.rows;
+}
+
+const addViaje = async (destino, presupuesto) => {
+    const consulta = "INSERT INTO viajes (destino, presupuesto) VALUES ($1, $2)";
+    await pool.query(consulta, [destino, presupuesto]);
+}
+
+const modificarViaje = async (id, destino, presupuesto) => {
+    const consulta = "UPDATE viajes SET destino = $1, presupuesto = $2 WHERE id = $3 RETURNING *";
+    const result = await pool.query(consulta, [destino, presupuesto, id]);
+
+    if(result.rowCount == 0){
+        throw {code: 404, message: "No se encontró el viaje con ese id"}
+    }
+
+    return result.rows
+}
+
+const eliminarViaje = async (id) => {
+    const consulta = "DELETE FROM viajes WHERE id = $1";
+    const result =await pool.query(consulta, [id]);
+    if(result.rowCount == 0){
+        throw {code: 404, message: "No se encontró el viaje con ese id"}
+    }
+}
+
+const getViajePorId = async (id) =>{
+    const consulta = "SELECT * FROM viajes WHERE id = $1";
+    const result = await pool.query(consulta, [id]);
+    console.log(result)
+
+    if(result.rowCount == 0){
+        throw {code: 404, message: "No se encontró el viaje con ese id"}
+    }
+    return result.rows;
+}
+
+
+
+//Consultas de los equipamientos
+
+const getEquipamientos = async () => {
+    const consulta = 'SELECT * FROM equipamiento';
+    const result = await pool.query(consulta);
+    return result.rows;
+}
+
+const getEquipamientoPorId = async (id) => {
+    const consulta = 'SELECT * FROM equipamiento WHERE id = $1';
+    const result = await pool.query(consulta, [id]);
+    if(result.rowCount == 0){
+        throw {code: 404, message: "No se encontró el equipamiento con ese id"}
+    }
+    return result.rows;
+}
+
+const addEquipamiento = async (nombre) => {
+    const consulta = "INSERT INTO equipamiento (nombre) VALUES ($1)";
+    await pool.query(consulta, [nombre]);
+}
+
+const modificarEquipamiento = async (id, nombre) => {
+    const consulta = "UPDATE equipamiento SET nombre = $1 WHERE id = $2 RETURNING *";
+    const result = await pool.query(consulta, [nombre, id]);
+    if(result.rowCount == 0){
+        throw {code: 404, message: "No se encontró el equipamiento con ese id"}
+    }
+    return result.rows;
+}
+
+const eliminarEquipamiento = async (id) => {
+    const consulta = "DELETE FROM equipamiento WHERE id = $1";
+    const result = await pool.query(consulta, [id]);
+    if(result.rowCount == 0){
+        throw {code: 404, message: "No se encontró el equipamiento con ese id"}
+    }
+}
+
+
+module.exports = { getDate, getViajes, addViaje, modificarViaje, eliminarViaje, getViajePorId,
+    getEquipamientos, addEquipamiento, modificarEquipamiento, eliminarEquipamiento, getEquipamientoPorId
+ };
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { getDate } = require('./consultas');
+const { getDate, getViajes, addViaje, modificarViaje, eliminarViaje, getViajePorId, 
+    getEquipamientos, addEquipamiento, modificarEquipamiento, eliminarEquipamiento, getEquipamientoPorId
+ } = require('./consultas');
 
 const port = process.env.PORT || 3000;
 
@@ -16,3 +18,116 @@ app.get('/', async (req, res) => {
     const date = await getDate();
     res.send(date);
 });
+
+//Rutas para los viajes
+
+app.get("/viajes", async (req, res)=>{
+    const viajes = await getViajes();
+    res.json(viajes)
+})
+
+app.post("/viajes", async (req, res)=>{
+    const { destino, presupuesto } = req.body;
+    try {
+        await addViaje(destino, presupuesto);
+        res.status(201).send("Viaje añadido correctamente");
+    } catch (error) {
+        if(error.code == 23502){
+            res.status(500).send("Error al añadir el viaje: Se necesita que añadas el valor de todos los campos");
+        }
+        else{
+            res.status(500).send("Error al añadir el viaje" + error.detail);
+        }
+    }
+})
+
+app.put("/viajes/:id" , async (req, res)=>{
+    const { destino, presupuesto } = req.body;
+    const { id } = req.params;
+    try {
+        const viajeModificado = await modificarViaje(id, destino, presupuesto);
+        res.status(200).json(viajeModificado);
+    } catch (error) {
+        res.status(error.code).send(error.message);
+    }
+})
+
+app.delete("/viajes/:id" , async (req, res)=>{
+    const { id } = req.params;
+    try {
+        await eliminarViaje(id);
+        res.status(200).send("Viaje eliminado correctamente");
+    } catch (error) {
+        res.status(error.code).send(error.message);
+    }
+})
+
+app.get("/viajes/:id" , async (req, res)=>{
+    const { id } = req.params;
+    try {
+        const viaje = await getViajePorId(id);
+        res.status(200).json(viaje);
+    } catch (error) {
+        res.status(error.code).send(error.message);
+    }
+})
+
+
+//Rutas para los equipamientos
+
+app.get("/equipamientos", async (req, res)=>{
+    const equipamientos = await getEquipamientos();
+    res.json(equipamientos)
+})
+
+app.get("/equipamientos/:id" , async (req, res)=>{
+    const { id } = req.params;
+    try {
+        const equipamiento = await getEquipamientoPorId(id);
+        res.status(200).json(equipamiento);
+    } catch (error) {
+        res.status(error.code).send(error.message);
+    }
+})
+
+app.post("/equipamientos", async (req, res)=>{
+    const { nombre } = req.body;
+    if(!nombre){
+        res.status(500).send("Error al añadir el equipamiento: Se necesita que añadas el valor de todos los campos");
+    }else{
+        try {
+            await addEquipamiento(nombre);
+            res.status(201).send("Equipamiento añadido correctamente");
+        } catch (error) {
+            if(error.code == 23502){
+                res.status(500).send("Error al añadir el equipamiento: Se necesita que añadas el valor de todos los campos");
+            }
+            else{
+                res.status(500).send("Error al añadir el equipamiento" + error.detail);
+            }
+        }
+    }
+
+
+})
+
+app.put("/equipamientos/:id" , async (req, res)=>{
+    const { nombre } = req.body;
+    const { id } = req.params;
+    try {
+        const equipamientoModificado = await modificarEquipamiento(id, nombre);
+        res.status(200).json(equipamientoModificado);
+    } catch (error) {
+        res.status(error.code).send(error.message);
+    }
+})
+
+app.delete("/equipamientos/:id" , async (req, res)=>{
+    const { id } = req.params;
+    try {
+        await eliminarEquipamiento(id);
+        res.status(200).send("Equipamiento eliminado correctamente");
+    } catch (error) {
+        res.status(error.code).send(error.message);
+    }
+})
